476b7f391a5697bfd2604353a1df192ae1705991
Felix Schlitter <felixschlitter@protonmail.com>
2018-11-10 10:08:31 +1300

    Add type annotation to `mkHandler`

--- src/UV/Types.purs
+++ src/UV/Types.purs
@@ -12,8 +12,11 @@ module UV.Types
 import Prelude
 
 import Control.Monad.Except (ExceptT(..), withExceptT)
+import Data.Either (Either)
+import Data.Symbol (class IsSymbol, SProxy)
 import Data.Variant (Variant, inj)
 import Effect (Effect)
+import Prim.Row (class Cons)
 import Unsafe.Coerce (unsafeCoerce)
 
 foreign import data SockAddrIn :: Type
@@ -31,6 +34,14 @@ type Port = Int
 type Handler es a =
   ExceptT (Variant es) Effect a
 
+mkHandler
+  :: ∀ ls e l a f es
+   . Functor f
+  => Cons l e ls es
+  => IsSymbol l
+  => SProxy l
+  -> f (Either e a)
+  -> ExceptT (Variant es) f a
 mkHandler sproxy x =
   withExceptT (inj sproxy) $
     ExceptT x
